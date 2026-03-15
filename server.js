2bf00d7874cacb5667f09b78fa56ba7975ae01b2
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-03-15 20:54:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-function Dobluyusss(){
+export default function Dobluyusss(){
  new Promise((resolve, reject) => {
     let Alhaji = true
+    setTimeout(() => {
    if (Alhaji) {
     resolve(" OKay so wetin hapen");
     
    } else {
    reject(" You dey go naw")
    }
+   timeout});
  })
  
 
+    
 }
