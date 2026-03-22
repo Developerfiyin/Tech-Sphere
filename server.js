ede0943371c585ed216a214cdf38021b4cb0ec5d
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-03-22 18:23:26 +0100

    Correct post Api and it was sucessfully

--- server.js
+++ server.js
@@ -11,39 +11,51 @@
 //   });
 // }
 
-try {
-  console.log(" what are you looking for ");
-  console.log("okay jaw naw, i had a dream ");
-  console.log(okay);
-} catch (err) {
-  console.error("Your error is ", err);
-} finally {
-  console.log("i will execte anyways");
-}
+// try {
+//   console.log(" what are you looking for ");
+//   console.log("okay jaw naw, i had a dream ");
+//   console.log(okay);
+// } catch (err) {
+//   console.error("Your error is ", err);
+// } finally {
+//   console.log("i will execte anyways");
+// }
 // async function ShartMehn() {
 //   const DobluyusssInthechat = await Dobluyusss();
 //   console.log(DobluyusssInthechat);
 // }
 // ShartMehn();
 
-//import express from "express";
-const express = require("express");
+import express from "express";
+//const express = require("express");
 //import cors from "cors";
 const app = express();
 
 //app.use(cors());
 app.use(express.json());
-let students = [];
+
 
 app.get("/", (req, res) => {
-  res.send(" Hello World !!!!!!!!!!!!");
+  res.send(" Hello World !!!!!!!!!!");
 });
+let students = [];
 
 app.get("/students", (req, res) => {
   res.send("Dobluyuss in the chat Mehn !");
 });
 
-
+app.post("/students", (req, res) => {
+  const {name , email, age} = req.body
+  if (!name || !email || !age) {
+    return res.status(400).send
+    ("All files are required!")
+  }
+ 
+   const student = {name ,email, age}
+   students.push(student)
+  
+   res.status(200).send(" Product Api created sucessfully!")
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000 ");
