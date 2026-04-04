d5c469fcb6d9f86c33960dbf9a352df1ebcb2d68
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-04-04 23:55:33 +0100

    Create a post route to create a new book

--- server.js
+++ server.js
@@ -26,20 +26,34 @@ const books = [
   { id: 3, title: "1984", author: "George Orwell" },
 ]
 
-const newbooks = {id: books.length + 1, title , author} = req.body
-if (!title || !author) {
+
+
+app.post("/books", (req,res) => {
+
+  const newbooks = {id: books.length + 1, title , author} = req.body
+ if (!title || !author) {
   res.status(400).json({"message": "fill it our correctly!!!!!!!!!!!!!"})
   
-}
-console.log(books);
+ }
+ console.log(books);
 
-else if(title && author) {
+ const book =  (title && author) {
+  todos.push(books))
+ }
+  {
   res.status(201).json(newbooks)
 }
+
+
+}
+)
+
 app.get("/books", (req, res) => {
   res.status(200).json({books})
   books.push(newbooks);
 });
+
+
 //   return new Promise((resolve, reject) => {
 //     setTimeout(() => {
 //       if (Dobluyusss) {
